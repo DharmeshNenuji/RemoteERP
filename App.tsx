a6e575a722728e544de301e15d508ffe9f597b2f
Yogesh Solanki <Solankiyogesh3500@gmail.com>
2025-01-05 23:50:37 +0530

    feat: added splashscreen and icons in ios and android

--- App.tsx
+++ App.tsx
@@ -3,17 +3,19 @@ import './src/i18n/i18n'
 import {NavigationContainer} from '@react-navigation/native'
 import React from 'react'
 import {GestureHandlerRootView} from 'react-native-gesture-handler'
+import SplashScreen from 'react-native-splash-screen'
 
 import {AppLoader} from '@/Components'
-import {AppNavigation} from '@/Router'
+import Loader from '@/Components/AppLoader/Loader'
+import {AppNavigation, navigationRef} from '@/Router'
 import {CommonStyle} from '@/Theme'
 
 export default () => {
   return (
     <GestureHandlerRootView style={CommonStyle.flex}>
-      <NavigationContainer>
+      <NavigationContainer ref={navigationRef} onReady={() => SplashScreen.hide()}>
         <AppNavigation />
-        <AppLoader />
+        <AppLoader ref={Loader.setLoader} />
       </NavigationContainer>
     </GestureHandlerRootView>
   )
--- src/Screens/Main/DashBoardScreen/DashBoardScreen.tsx
+++ src/Screens/Main/DashBoardScreen/DashBoardScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useTranslation} from 'react-i18next'
-import {ScrollView, StyleSheet} from 'react-native'
+import {ScrollView, StyleSheet, View} from 'react-native'
 
 import {AppContainer} from '@/Components'
 import AppHeader from '@/Components/AppHeader/AppHeader'
@@ -21,6 +21,7 @@ export default () => {
             <ListRenderWithTitle isNormalView={index === 0} key={title} data={data} title={title} />
           )
         })}
+        <View style={styles.noView} />
       </ScrollView>
     </AppContainer>
   )
@@ -28,7 +29,9 @@ export default () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginBottom: verticalScale(16),
     padding: scale(15)
+  },
+  noView: {
+    height: verticalScale(50)
   }
 })
